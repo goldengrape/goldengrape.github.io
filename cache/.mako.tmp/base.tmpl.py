df4e5cafa529b5c39c8bc6e7dc0151878b55f3fc
GoldenGrape <goldengrape@goldengrape.org>
2018-03-28 23:24:05 +0800

    Nikola auto commit.
    Nikola version: 7.8.10

--- cache/.mako.tmp/base.tmpl.py
+++ cache/.mako.tmp/base.tmpl.py
@@ -5,7 +5,7 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1522118559.246788
+_modified_time = 1522250641.402035
 _enable_loop = True
 _template_filename = 'themes/lanyon/templates/base.tmpl'
 _template_uri = 'base.tmpl'
@@ -40,25 +40,25 @@ def render_body(context,**pageargs):
         _mako_get_namespace(context, 'base')._populate(_import_ns, ['*'])
         _mako_get_namespace(context, 'header')._populate(_import_ns, ['*'])
         _mako_get_namespace(context, 'footer')._populate(_import_ns, ['*'])
-        def content():
-            return render_content(context._locals(__M_locals))
-        template_hooks = _import_ns.get('template_hooks', context.get('template_hooks', UNDEFINED))
-        header = _mako_get_namespace(context, 'header')
-        body_end = _import_ns.get('body_end', context.get('body_end', UNDEFINED))
-        js_date_format = _import_ns.get('js_date_format', context.get('js_date_format', UNDEFINED))
-        lang = _import_ns.get('lang', context.get('lang', UNDEFINED))
-        search_form = _import_ns.get('search_form', context.get('search_form', UNDEFINED))
         def extra_head():
             return render_extra_head(context._locals(__M_locals))
+        lanyon_subtheme = _import_ns.get('lanyon_subtheme', context.get('lanyon_subtheme', UNDEFINED))
+        momentjs_locales = _import_ns.get('momentjs_locales', context.get('momentjs_locales', UNDEFINED))
+        js_date_format = _import_ns.get('js_date_format', context.get('js_date_format', UNDEFINED))
+        template_hooks = _import_ns.get('template_hooks', context.get('template_hooks', UNDEFINED))
+        set_locale = _import_ns.get('set_locale', context.get('set_locale', UNDEFINED))
+        def content():
+            return render_content(context._locals(__M_locals))
+        body_end = _import_ns.get('body_end', context.get('body_end', UNDEFINED))
+        header = _mako_get_namespace(context, 'header')
+        base = _mako_get_namespace(context, 'base')
         def extra_js():
             return render_extra_js(context._locals(__M_locals))
-        footer = _mako_get_namespace(context, 'footer')
-        momentjs_locales = _import_ns.get('momentjs_locales', context.get('momentjs_locales', UNDEFINED))
-        lanyon_subtheme = _import_ns.get('lanyon_subtheme', context.get('lanyon_subtheme', UNDEFINED))
-        set_locale = _import_ns.get('set_locale', context.get('set_locale', UNDEFINED))
         date_fanciness = _import_ns.get('date_fanciness', context.get('date_fanciness', UNDEFINED))
-        base = _mako_get_namespace(context, 'base')
+        search_form = _import_ns.get('search_form', context.get('search_form', UNDEFINED))
+        lang = _import_ns.get('lang', context.get('lang', UNDEFINED))
         messages = _import_ns.get('messages', context.get('messages', UNDEFINED))
+        footer = _mako_get_namespace(context, 'footer')
         __M_writer = context.writer()
         __M_writer('\n')
         __M_writer('\n')
---
+++ posts/bulabula/折叠键盘支撑.md
@@ -0,0 +1,39 @@
+<!--
+.. title: 折叠键盘支撑
+.. slug: foldable-keyboard-supp
+.. date: 2018-3-28 23:00 UTC+08:00
+.. tags: life science
+.. category:
+.. link:
+.. description:
+.. type: text
+-->
+
+这几天不知道为什么弄伤了手腕, 在笔记本键盘上敲字的时候手腕更加不舒服.
+
+于是先从thingverse上找到了早已看中的[护腕](https://www.thingiverse.com/thing:403001)打印了一副戴上.
+![护腕](https://i.loli.net/2018/03/28/5abbb0913f1a9.jpeg)
+PLA遇到60度以上的热水就软化, 这个特性还真是挺好用的. 虽然60度的水很烫, 但是拿出来的护腕放在手上并不烫, 可以趁热捏成合适的形状, 把手腕固定在功能位上.
+
+但仍然不满足. 因为在打字时手腕是放平的, 所以相对于功能位有一点内旋. 实际上平面的键盘总是不能让手腕很舒服.
+
+于是改造键盘.
+<!-- TEASER_END -->
+
+手里有一款B.O.W. 的蓝牙折叠键盘, 型号应该是"B.O.W HB188". 正常使用时折叠方式是谷折叠, 但也可以山折叠, 使键盘离开桌面.
+
+于是做了一组卡槽, 将键盘两端用线拉住, 键盘就形成了一个"人"字形, 可以支撑在桌面上.
+![WechatIMG61](https://i.loli.net/2018/03/28/5abbb0928b302.jpeg)
+支撑的高度用线长来调节就可以.
+这样手在打字时, 手腕就不必内旋了.
+![WechatIMG62](https://i.loli.net/2018/03/28/5abbb0914b2e8.jpeg)
+
+打印出4个组件固定会比较稳定.
+![WechatIMG60](https://i.loli.net/2018/03/28/5abbb09180420.jpeg)
+
+之前这款折叠键盘必须放在桌面上使用, 无法放在腿上, 现在也可以直接放在推腿上用了.
+![WechatIMG63](https://i.loli.net/2018/03/28/5abbb09273b72.jpeg)
+
+零件不大, 方便收纳.
+
+[下载打印零件](https://www.thingiverse.com/thing:2843000)
